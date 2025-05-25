c975a078c7188c286b5c5d48f3b6f1131bee647c
Code1Twilight <ronnelerrolsantos0@gmail.com>
2025-05-25 09:54:01 +0800

    Update confession.py

--- confession.py
+++ confession.py
@@ -55,5 +55,5 @@ def submit():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        init_db()
+        print("Database tables created.")
     app.run(debug=True)
